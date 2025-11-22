f0c0b6e015056dbd6cc004e58b7d0250c4a42c58
Wojtek <w.borak@infomex.pl>
2025-11-22 13:26:02 +0100

    port change

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ WORKDIR /usr/src/app
 COPY package.json .
 RUN npm install --production
 COPY . .
-ENV PORT=8080
-EXPOSE 8080
+ENV PORT=3000
+EXPOSE 3000
 CMD [ "node", "server.js" ]
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { v4: uuidv4 } = require('uuid');
 const WebSocket = require('ws');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 // Serve static frontend
 app.use(express.static(path.join(__dirname, 'public')));
